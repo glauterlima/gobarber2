2b46babe66dcd47bcbe498e3a0c045f1f20813f4
Glauter Santos <glauterlima@gmail.com>
2020-04-05 12:01:45 -0300

    Configurando estrutura inicial

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('./routes');
+
+class App {
+  constructor() { //classe carregada 
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server; //Estância de App e método disponível
