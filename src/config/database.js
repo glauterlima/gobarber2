919dcc2eecc8afdb235a9581e05adf12b5cf8d59
Glauter Santos <glauterlima@gmail.com>
2020-04-05 18:32:59 -0300

    Finalizada configuração do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'Raul596363',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
